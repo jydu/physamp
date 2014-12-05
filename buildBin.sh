e6565f282195a711225cb759c6bd3dadd88c2fce
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2014-12-05 12:21:14 +0100

    Fixed dir name.

--- buildBin.sh
+++ buildBin.sh
@@ -2,6 +2,6 @@
 arch=x86_64 #i686
 version=0.1.0-1
 
-strip bppSuite/bppalnoptim
-tar cvzf physamp-${arch}-bin-static-${version}.tar.gz PhySamp/bppalnoptim
+strip physamp/bppalnoptim
+tar cvzf physamp-${arch}-bin-static-${version}.tar.gz physamp/bppalnoptim
 
